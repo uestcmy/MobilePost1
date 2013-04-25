7fc80c9dc78520fac19116792a6e4284b194dd2d
uestcmy <mengyangmy@gmail.com>
2013-04-26 00:49:13 +0800

    add EditText module and TextView.by my.

--- gen/com/example/mobilepost1/R.java
+++ gen/com/example/mobilepost1/R.java
@@ -26,8 +26,14 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080006;
+        public static final int etId=0x7f080002;
+        public static final int etInput=0x7f080005;
+        public static final int etMobile=0x7f080004;
         public static final int textView1=0x7f080000;
+        public static final int textView3=0x7f080001;
+        public static final int textViewMobile=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,8 +44,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button1=0x7f050004;
         public static final int hello_Taipei=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int id=0x7f050006;
+        public static final int mobile=0x7f050007;
+        public static final int toInput=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/example/mobilepost1/MainActivity.java
+++ src/com/example/mobilepost1/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.mobilepost1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
@@ -10,13 +17,42 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViews();
+		//findviews2();
 	}
 
+	private void findViews(){
+		Button button1 = (Button)findViewById(R.id.button1);
+		button1.setOnClickListener(new View.OnClickListener() {
+		@Override
+			public void onClick(View v) {
+				CharSequence text = ((Button)v).getText();
+				Toast t1 = Toast.makeText(MainActivity.this, text,Toast.LENGTH_SHORT);
+				t1.show();
+			}
+		});
+	}
+
+		public  void findviews2(){
+			final EditText etInput = (EditText) findViewById(R.id.etInput);
+			etInput.setOnKeyListener(new View.OnKeyListener() {
+				
+				@Override
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
+						Toast t1 = Toast.makeText(MainActivity.this, etInput.getText(), Toast.LENGTH_LONG);
+						t1.show();
+						return true;
+					}
+					return false;
+				}
+			});
+		}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
